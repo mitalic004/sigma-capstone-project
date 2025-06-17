6d0e1ababa92ad406f2b9a00f279f7eeb5ed498e
Mitali <mitalichavan004@gmail.com>
2025-06-17 19:41:51 +0100

    Add player and dragon dictionaries with stats

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+
+player = {
+    "HP": 100,
+    "ATK": 15,
+    "DEF": 30,
+    "Buff_ATK": 0,
+    "Buff_DEF": 0
+}
+
+dragon = {
+    "HP": 100,
+    "ATK": 15,
+    "DEF": 30,
+}
